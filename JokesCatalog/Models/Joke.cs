d24f4dc0208b36bfe2f88b04abca8729ace93493
Devon Blackbeard <32315720+devonblackbeard@users.noreply.github.com>
2021-10-23 17:15:20 -0600

    Add project files.

---
+++ JokesCatalog/Models/Joke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesCatalog.Models
+{
+    public class Joke
+    {
+        public Joke()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        [Display(Name ="Date Created")]
+        public DateTime DateCreated { get; set; }
+    }
+}
